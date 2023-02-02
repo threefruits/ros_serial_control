3fb05ec5bdfe8ca563228d4b0321f4a779d09868
threefruits <anxingxiao@gmail.com>
2023-02-02 14:06:24 +0800

    update

--- my_serial_node/src/my_serial_node.cpp
+++ my_serial_node/src/my_serial_node.cpp
@@ -5,14 +5,14 @@
 #include <geometry_msgs/Twist.h>
 
 serial::Serial ros_ser;
-#define	sBUFFERSIZE	10//send buffer size 串口发送缓存长度
-unsigned char s_buffer[sBUFFERSIZE];//发送缓存
+#define	sBUFFERSIZE	10//send buffer size
+unsigned char s_buffer[sBUFFERSIZE];
 int l_v=0;
 int r_v=0;
 int l_d=1;
 int r_d=1;
-const float d_between=0.165;//半轮间距
-const float d=0.125;//轮子半径
+const float d=0.165;//distance between two wheels
+const float r=0.125;//radius of wheels
 
 typedef union{
 	unsigned char cvalue[4];
@@ -21,18 +21,18 @@ typedef union{
 
 void cmd_to_serial(const geometry_msgs::Twist msg)
 {
-    r_v=27*(msg.linear.x + msg.angular.z * d_between)/(3.1416*d);
-    l_v=27*(msg.linear.x - msg.angular.z * d_between)/(3.1416*d);
-    if(r_v>=0)
+    r_v=27*(msg.linear.x + msg.angular.z * d)/(3.1416*r);
+    l_v=27*(msg.linear.x - msg.angular.z * d)/(3.1416*r);
+    if(r_v>=0) //positive
     {
         r_d=0;
         r_v=r_v;
-    }             //电机正转
-    else
+    }             
+    else //negetive
     {
         r_d=1;
         r_v=-r_v;
-    }           //电机反转
+    }           
 
     if(l_v>=0)
     {
@@ -70,18 +70,20 @@ void data_to_serial(const int left, const int right, const int left_d, const int
      s_buffer[9]=0x00;
      ros_ser.write(s_buffer,sBUFFERSIZE);
 }
-//回调函数
-void callback(const geometry_msgs::Twist& msg){
+
+void callback(const geometry_msgs::Twist& msg) //callback function
+{  
      cmd_to_serial(msg);
      data_to_serial(l_v,r_v,l_d,r_d);
  }
+
 int main (int argc, char** argv){
      ros::init(argc, argv, "my_serial_node");
      ros::NodeHandle n;
-     //订阅主题command
+
      ros::Subscriber command_sub = n.subscribe("/cmd_vel", 1000, callback);
-     //发布主题sensor
      ros::Publisher sensor_pub = n.advertise<std_msgs::String>("sensor", 1000);
+
      try
      {
          ros_ser.setPort("/dev/ttyUSB0");
@@ -90,6 +92,7 @@ int main (int argc, char** argv){
          ros_ser.setTimeout(to);
          ros_ser.open();
      }
+
      catch (serial::IOException& e)
      {
          ROS_ERROR_STREAM("Unable to open port ");
@@ -106,10 +109,8 @@ int main (int argc, char** argv){
          if(ros_ser.available()){
              ROS_INFO_STREAM("Reading from serial port");
              std_msgs::String serial_data;
-             //获取串口数据
              serial_data.data = ros_ser.read(ros_ser.available());
              ROS_INFO_STREAM("Read: " << serial_data.data);
-             //将串口数据发布到主题sensor
              sensor_pub.publish(serial_data);
          }
          loop_rate.sleep();
